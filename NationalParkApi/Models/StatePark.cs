a2b04e396a087d9dcc29136f8beeec33629912b2
Jake Elsberry <jelsberry96@gmail.com>
2023-10-27 13:24:26 -0700

    add StatePark model

---
+++ NationalParkApi/Models/StatePark.cs
@@ -0,0 +1,10 @@
+namespace NationalParkApi.Models
+{
+  public class StatePark 
+  {
+    public int StateParkId { get; set; }
+    public string Name { get; set; }
+    public string StateCode { get; set; }
+    public int YearCreated { get; set;}
+  }
+}
